c44ded664e61a98df78c776aa316d22a982e7a0f
Rudy Samuel <register.rusa@gmail.com>
2026-05-14 23:59:46 +0800

    chore: bump version to 1.11.0

--- frontend/src/lib/chords.ts
+++ frontend/src/lib/chords.ts
@@ -204,6 +204,7 @@ class ResponsiveHtmlFormatter {
     if (l.type === 'comment') {
       const firstItem = l.items[0];
       const content = (firstItem && 'content' in firstItem ? (firstItem as ChordSheetJS.Comment).content : 
+                     // eslint-disable-next-line @typescript-eslint/no-explicit-any
                      (firstItem && 'lyrics' in firstItem ? (firstItem as any).lyrics : '')) || '';
       
       if (SECTION_RE.test(content.trim())) {
